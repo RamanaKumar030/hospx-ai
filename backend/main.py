574e071a81d9bc20980b8a0dea4d5165d12e6abe
RamanaKumar1234 <kavitharamana313@gmail.com>
2026-05-18 16:32:47 +0530

    update backend files for deployment

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 import requests
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -92,3 +93,5 @@ def nearby_hospitals(request: LocationRequest):
                 {"name": "Emergency Hospital (Offline Mode)", "type": "hospital"}
             ]
         }
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)

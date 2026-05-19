94544fea0d05096a9e44da969473f0204d71c6ef
RamanaKumar1234 <kavitharamana313@gmail.com>
2026-05-19 10:32:28 +0530

    update frontend backend api url

--- vercel-frontend/app/page.tsx
+++ vercel-frontend/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 
-const API = "http://127.0.0.1:8000";
+const API = "https://hospx-ai-3.onrender.com";
 
 export default function Home() {
   const [symptoms, setSymptoms] = useState("");
